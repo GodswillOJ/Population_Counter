91595f94b706cf8a08fcb9726f75b88988dbc6c6
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-02-15 18:11:57 +0100

    cx

--- Backend/src/userRoute/User.js
+++ Backend/src/userRoute/User.js
@@ -14,6 +14,6 @@ userRouter.post('/login', userController.LoginVerify);
 
 userRouter.get('/dashboard', isAuthenticated, userController.fetchUserData); // Route to fetch user data for the dashboard
 
-userRouter.get('/', isAuthenticated, userController.Home);
+userRouter.get('/', isAuthenticated, userController.fetchUserData);
 
 export { userRouter };
